8e78a8bd40bf2a862c4cd5eeb6390997d4fc46e2
mugur <mugurr@mail.ru>
2024-11-09 15:45:01 +0500

    Добавил текс

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Изучаем Git");
+        System.out.println("Звук исчез);
+    }
+}
+
+// git config --global user.name "Your Name"
+// git config --global user.email "Your email
